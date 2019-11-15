130e0766d8c8e7085b452fb01142826e31fd0bce
Fabian FACINOU <20191076@b15-12.local>
2019-11-15 11:56:39 +0100

    oav.02 to csv2json

---
+++ main.js
@@ -0,0 +1,14 @@
+const { duplicate } = require('./streambox')
+const { transform } = require('./streambox')
+const { csv2json } = require('./streambox')
+const { upperCase } = require('voca')
+
+//duplicate('./Ch0pin.jpg')
+
+// transform('./test.txt', /Ch0pin/g, function(strToReplace){
+//     return strToReplace.toUpperCase()
+// })
+
+//transform('./test.txt', /Ch0pin/g, upperCase, false)
+
+// csv2json('./Comp0ser.csv')
---
+++ streambox.js
@@ -0,0 +1,99 @@
+const fs = require('fs')
+const path = require('path')
+const { Transform } = require('stream')
+
+function getDuplicateName(filename){
+    const ext = path.extname(filename)
+    const base = path.basename(filename, ext)
+    return `${base}.copy${ext}`
+}
+
+function duplicate(filename){
+    const outputFileName = getDuplicateName(filename)
+    const file = fs.createReadStream(filename)
+    const newFile = fs.createWriteStream(outputFileName)
+    file.pipe(newFile)
+
+    file.on('end', () => {
+        console.log(`File ${filename} successfully duplicated!!`)
+    })
+}
+
+function transform(filename, re, cb, inStdout = true){
+    if (inStdout){
+        let content = ''
+
+        rstream.on('data', chunk => {
+            content += chunk.toString().replace(re, cb)
+        })
+
+        rstream.on('end', () => {
+            console.log(content)
+        })
+    }else{
+        const outputFileName = getDuplicateName(filename)
+        const rstream = fs.createReadStream(filename)
+        const wstream = fs.createWriteStream(outputFileName)
+        const tstream = new Transform({
+        transform(chunk, encoding, callback){
+                this.push(chunk.toString().replace(re,  cb))
+                callback()
+            }
+        })
+        rstream.pipe(tstream).pipe(wstream)
+    }
+}
+
+function getJSON(filename){
+    const ext = path.extname(filename)
+    const base = path.basename(filename, ext)
+    return `${base}.json`
+}
+
+function csv2json(filename){
+    const outputFileName = getJSON(filename)
+    const rstream = fs.createReadStream(filename)
+    let obj = {}
+    let tab
+    let activities
+    let date
+    let t = ''
+    rstream.on('data', chunk => {
+        chunk = chunk.toString().replace(/\r/g, '')
+        tab = chunk.toString().split(/\n/)
+        keys = tab[0]
+        values = tab[1]
+        splitKeys = keys.split(';')
+        splitValues = values.split(';')
+        
+        for (const elt in splitKeys){
+            if(splitKeys[elt] === 'birth' | splitKeys[elt] === 'death'){
+                date = splitValues[elt]
+                date = date.split('/')
+                splitValues[elt] = `${date[2]}-${date[1]}-${date[0]}`
+                obj[splitKeys[elt]] = splitValues[elt]
+            }
+            if (splitKeys[elt] === 'activities'){
+                activities = splitValues[elt].split(',')
+                splitValues[elt] = activities
+                obj[splitKeys[elt]] = splitValues[elt]
+            }else{
+                obj[splitKeys[elt]] = splitValues[elt]
+            }
+        }
+        
+    })
+
+    rstream.on('end', () => {
+        fs.writeFile(outputFileName, JSON.stringify(obj), function (err) {
+            if (err) throw err;
+            console.log('Created JSON!');
+          });
+    })
+}
+
+module.exports = {
+    duplicate,
+    transform,
+    csv2json
+}
